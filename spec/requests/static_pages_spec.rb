d5c215710c2966ba7c63a6b58c621ddb06ecca2a
David Kroner <kroner.david@gmail.com>
2013-06-03 22:26:58 -0400

    Added full title dynamically on each page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,8 +12,14 @@ describe "Static pages" do
 		it "should have the right title" do
   			visit '/static_pages/home'
   			page.should have_selector('title',
-                :text => "Ruby on Rails Tutorial Sample App | Home")
+                :text => "Ruby on Rails Tutorial Sample App")
 		end
+
+		it "should not have a custom page title" do
+      		visit '/static_pages/home'
+      		page.should_not have_selector('title', 
+      			:text => '| Home')
+    	end
 	end
 
 	describe "Help page" do
@@ -39,6 +45,19 @@ describe "Static pages" do
 			visit '/static_pages/about'
 			page.should have_selector('title',
 				:text => "| About Us")
+		end
+	end
+
+	describe "Contact" do
+		it "should have the content 'Contact Us' " do
+			visit '/static_pages/contact'
+			page.should have_selector('h1',
+				:text => "Contact Us")
+		end
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			page.should have_selector('title',
+				:text => "| Contact Us")
 		end
 	end
 end
