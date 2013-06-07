a172d8a597a075711e957af5cf54929be65dbf03
David Kroner <kroner.david@gmail.com>
2013-06-06 22:29:15 -0400

    refactor  rspec for the homepage tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
 describe "Static pages" do
-	describe "Home page" do
 
-		it "should have the content 'Sample App' " do
-			visit root_path
-			page.should have_selector('h1',
-				:text => "Sample App")
+	subject {page}
+
+	describe "Home page" do
+		before { visit root_path }
+		it { should have_selector('h1', :text => "Sample App") }
+		it { should have_selector('title', text: "Ruby on Rails Sample App")}
+		it {should_not have_selector 'title', text: '| Home'}
 		end
 
-		it "should have the right title" do
-  			visit root_path
+		it "should have the right title" do  			
   			page.should have_selector('title',
                 :text => "Ruby on Rails Tutorial Sample App")
 		end
 
-		it "should not have a custom page title" do
-      		visit root_path
+		it "should not have a custom page title" do      		
       		page.should_not have_selector('title', 
       			:text => '| Home')
     	end
