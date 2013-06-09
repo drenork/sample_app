439384ad364212a89e352556e05afe010bdebe18
David Kroner <kroner.david@gmail.com>
2013-06-09 10:39:44 -0400

    linked homepage to signup and dynamic title on signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ SampleApp::Application.routes.draw do
 
   # get "static_pages/home"
   root to: 'static_pages#home'
+
+  match 'signup', to: 'users#new'
   # get "static_pages/help"
   match 'help', to: 'static_pages#help'
   # get "static_pages/about"
